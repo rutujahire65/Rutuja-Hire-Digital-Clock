017bcd87a5526f34ba8df57fc79a20406e30d8a5
rutujahire65 <119806384+rutujahire65@users.noreply.github.com>
2023-05-20 13:38:11 +0530

    Add files via upload

---
+++ component/Basics/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = ({ filterItem, menuList, }) => {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='btn-group'>
+                    {
+                        menuList.map((curElem) => {
+                            return (
+                            <button className='btn-group__item'
+                                onClick={() => filterItem(curElem)}>{curElem}</button>
+                            )
+                        })
+                    }
+
+
+                    {/* <button className=' btn-group__item' onClick={() => filterItem('lunch')}>lunch</button>
+                    <button className=' btn-group__item' onClick={() => filterItem('evening')}>Evening</button>
+                    <button className=' btn-group__item' onClick={() => filterItem('dinner')}  >Dinner</button>
+                    <button className=' btn-group__item' onClicsetMenuDatak={() => (Menu)}>All</button> */}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
